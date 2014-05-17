076f44a5f245ed63051ca948248c6bc34edd0a38
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2014-05-17 11:51:26 +0430

    Added some new services

--- src/coffee/services.coffee
+++ src/coffee/services.coffee
@@ -1,11 +1,14 @@
 
 esmfamil.value 'firebaseRef', new Firebase "https://fiery-fire-2012.firebaseio.com/"
 
-esmfamil.factory 'people', ($firebase, firebaseRef) ->
-  peopleRef = firebaseRef.child 'people'
-  $firebase peopleRef
+esmfamil.factory 'players', ($firebase, firebaseRef) ->
+  playersRef = firebaseRef.child 'players'
+  $firebase playersRef
 
 esmfamil.factory 'games', ($firebase, firebaseRef) ->
   gamesRef = firebaseRef.child 'games'
   $firebase gamesRef
+
+esmfamil.factory 'self', ->
+  {}
 
--- www/js/services.js
+++ www/js/services.js
@@ -1,15 +1,19 @@
 esmfamil.value('firebaseRef', new Firebase("https://fiery-fire-2012.firebaseio.com/"));
 
-esmfamil.factory('people', function($firebase, firebaseRef) {
-  var peopleRef;
-  peopleRef = firebaseRef.child('people');
-  return $firebase(peopleRef);
+esmfamil.factory('players', function($firebase, firebaseRef) {
+  var playersRef;
+  playersRef = firebaseRef.child('players');
+  return $firebase(playersRef);
 });
 
 esmfamil.factory('games', function($firebase, firebaseRef) {
   var gamesRef;
   gamesRef = firebaseRef.child('games');
   return $firebase(gamesRef);
+});
+
+esmfamil.factory('self', function() {
+  return {};
 });
 
 //# sourceMappingURL=services.js.map
